d4453f1d8fdd56ac40aefda9e79e55b47ddd1ade
al_skipp <al_skipp@fastmail.fm>
2014-04-01 17:51:34 +0100

    Added example project

---
+++ Example/ColorWriterExample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ColorWriterExample
+//
+//  Created by Alan Skipp on 01/04/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/ColorWriterExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  ColorWriterExample
+//
+//  Created by Alan Skipp on 01/04/2014.
+//  Copyright (c) 2014 Alan Skipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
